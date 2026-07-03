f86f4744b350fffa1d0b2c40af466d7ca6cb76d5
Charlotte Schlenius <acschlenius@norsys.fr>
2026-07-03 18:23:58 +0200

    Deploy site

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -1,12 +1,14 @@
 export const quotes = [
   {
-    text: "Posséder la plus puissante des magies n’empêche pas d’apprécier un café beaucoup trop fort.",
-    source: "Le Cosmovarium",
+    text: "Les Humains n’étaient entrés qu’une seule fois en conflit avec la Terre de Galeb. Suite à la déculottée subie, les Humains ne se sont plus mis qu’à faire la guerre entre eux. Plus simple, plus pratique, moins de dégâts.",
+    source: "Le Chaos, jusqu'au cou !",
   },
   {
-    text: "Les dragons ont rarement le sens du timing, mais ils compensent par une confiance très personnelle en la gravité.",
+    text: "Un rameau de saule prit mentalement note de ce qui venait de se passer et entreprit d’ajouter cet événement singulier à la mémoire collective de la forêt.",
+    source: "Le Chaos, jusqu'au cou !",
   },
   {
-    text: "Certaines prophéties gagneraient à être relues par quelqu’un qui n’a pas dormi dans une bibliothèque depuis trois siècles.",
+    text: "Je m’attendais à quelque chose pour m’aider, pas une victime potentielle supplémentaire à gérer.",
+    source: "Le Chaos, jusqu'au cou !, le Hasard",
   },
 ];
